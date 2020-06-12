94aa2a844f75b8e7812306455692abe3ed69a76e
Rafael Oliveira <rusemy.10@gmail.com>
2020-06-12 18:53:14 -0300

    Criando o projeto Atividade04

---
+++ Atividade04/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Mirror;
+
+public class PlayerController : NetworkBehaviour {
+    
+    [SerializeField]private GameObject projetilPrefab;
+    [SerializeField]private Transform projetilPosition;
+
+    [SerializeField]private float moveSpeed = 0.1f;
+    [SerializeField]private float moveRotation = 10f;
+    
+    private void Start()
+    {
+        Material material = GetComponent<Renderer>().material;
+        material.color = new Color(Random.Range(0f, 255f), Random.Range(0f, 255f), Random.Range(0f, 255f));
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * moveSpeed);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * moveRotation, 0);
+
+        if (Input.GetKeyUp(KeyCode.Space))
+            CmdAtirar();
+    }
+
+    private void CmdAtirar()
+    {
+        GameObject projetil = Instantiate(projetilPrefab, projetilPosition.position, transform.rotation);
+        NetworkServer.Spawn(projetil);
+    }
+}
---
+++ Atividade04/Assets/Scripts/ProjetilController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Networking;
+
+public class ProjetilController : NetworkBehaviour {
+    
+    [SerializeField]private float moveSpeed = 20f;
+    
+    void Start()
+    {
+        Destroy(gameObject, 1);
+    }
+
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * moveSpeed);
+    }
+
+    
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other) {
+        NetworkServer.Destroy(gameObject);
+        NetworkServer.Destroy(other.gameObject);
+    }
+}
